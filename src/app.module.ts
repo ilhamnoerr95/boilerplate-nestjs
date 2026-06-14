235379214fd47ac50f74c5ed63d11a7895b4962f
ilhamnoerr95 <noerr.ilham@gmail.com>
2026-06-14 19:05:50 +0700

    [Feature] : exmple api

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { ExampleModule } from './modules/example/example.module';
+
 @Module({
-  imports: [],
+  imports: [ExampleModule],
   controllers: [AppController],
   providers: [AppService],
 })
